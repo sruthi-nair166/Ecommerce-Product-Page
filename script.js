adc0f07b5428d41311e628e61c9bf381eee1bc60
Sruthi <sruthivnair78@gmail.com>
2025-07-17 13:41:12 +0530

    Added thumbnail click and prev/next button functionality for product image

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const menuBtn = document.querySelector(".logo-nav div button");
 const menuLogo = document.querySelector(".logo-nav div button img")
 const menu = document.querySelector(".logo-nav nav");
 const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 const overlay = document.querySelector(".overlay");
 const productImage = document.querySelector(".product-image");
 const imageWrapper = document.querySelector(".image-wrapper");
 const cloneImageWrapper = document.querySelector(".clone");
 const closeBtn = document.querySelector(".close");
+const thumbnailsWrapper = document.querySelector(".thumbnails-wrapper");
+const thumbnails = document.querySelectorAll(".thumbnail");
 
 
 /* styling menu opening and closing in mobile layout */
@@ -44,22 +47,42 @@ window.addEventListener("resize", () => {
 /* opening the lightbox */
 
 function productImageClick() {
+  let clone = cloneImageWrapper.querySelector(".image-wrapper");
+
+  /* to prevent clone of imageWrapper from being created multiple times, and event listeners from being added too many times */
   if(!cloneImageWrapper.querySelector(".image-wrapper")) {
-      const clone = imageWrapper.cloneNode(true);
+      clone = imageWrapper.cloneNode(true);
       cloneImageWrapper.appendChild(clone);
 
+      const cloneProductImage = clone.querySelector(".product-image");
       const clonePrevBtn = clone.querySelector(".prev");
       const cloneNextBtn = clone.querySelector(".next");
-      clonePrevBtn.classList.add("zoom");
-      cloneNextBtn.classList.add("zoom");
+      const thumbnailsClone = clone.querySelectorAll(".thumbnail");
+
+      prevImage(cloneProductImage, clonePrevBtn, thumbnailsClone);
+      nextImage(cloneProductImage, cloneNextBtn, thumbnailsClone);
+
+      thumbnailSelector(thumbnailsClone, cloneProductImage);
+
+      closeLightBox(cloneProductImage, thumbnailsClone);
     }
+
+    const cloneProductImage = clone.querySelector(".product-image");
+    const thumbnailsClone = clone.querySelectorAll(".thumbnail");
+
+    /* to obtain current product Image and set the same in lightbox */
+    const currentProductImg = productImage.dataset.img;
+
+    imgSet(cloneProductImage, currentProductImg);
+    thumbnailImgSet(thumbnailsClone, currentProductImg);
 
     overlay.classList.add("zoom");
 
-    cloneImageWrapper.classList.add("zoom");
-
-    const cloneProductImage = clone.querySelector(".product-image");
+    cloneImageWrapper.classList.add("zoom"); 
     cloneProductImage.classList.add("zoom");
+    
+    clone.querySelector(".prev").classList.add("zoom");
+    clone.querySelector(".next").classList.add("zoom");
 }
 
 /* adding event listener on desktop layout and removing it in mobile layout */
@@ -81,12 +104,99 @@ function productImageListener() {
 productImageListener();
 window.addEventListener("resize", productImageListener);
 
-/* closing the lightbox */
+/* resetting product image to first image after closing light box */
 
-closeBtn.addEventListener("click", () => {
-  overlay.classList.remove("zoom");
-  cloneImageWrapper.classList.remove("zoom");
-})
+function reset(image, tbn) {
+  imgSet(image, "1");
+
+  thumbnailImgSet(tbn, 1);
+}
+
+/* function for closing the lightbox */
+
+function closeLightBox(clone, tbnClone) {
+  closeBtn.addEventListener("click", () => {
+    overlay.classList.remove("zoom");
+    cloneImageWrapper.classList.remove("zoom");
+
+    reset(clone, tbnClone);
+    reset(productImage, thumbnails);
+  });
+};
+
+function imgSet(img, num) {
+  img.setAttribute("src", `./images/image-product-${num}.jpg`);
+  img.setAttribute("data-img", `${num}`);
+}
+
+function thumbnailImgSet(tns, num) {
+  tns.forEach(tn => tn.classList.remove("selected"));
+  tns[Number(num)-1].classList.add("selected");
+}
+
+/* selecting images from the thumbnails */
+
+function thumbnailSelector(tns, image) {
+  tns.forEach(thumbnail => {
+  thumbnail.addEventListener("click", event => {
+
+    const number = event.currentTarget.dataset.img;
+
+    imgSet(image, number);
+    thumbnailImgSet(tns, number);
+    })
+  });
+}
+
+thumbnailSelector(thumbnails, productImage);
+
+
+/* previous and next buttons functionality */
+
+function prev(num) {
+  let number = Number(num);
+
+  if(number <= 1) return "1";
+  return String(number - 1);
+}
+
+function next(num) {
+  let number = Number(num);
+
+  if(number >= 4) return "4";
+  return String(number + 1);
+}
+
+function prevImage(image, btn, tbn) {
+  btn.addEventListener("click", () => {
+    let currentProductImg = image.dataset.img;
+
+    const prevImg = prev(currentProductImg);
+
+    imgSet(image, prevImg);
+
+    if(thumbnailsWrapper.style.display !== "none") {
+      thumbnailImgSet(tbn, prevImg);
+    }
+  }); 
+}
+
+function nextImage(image, btn, tbn) {
+  btn.addEventListener("click", () => {
+    let currentProductImg = image.dataset.img;
+
+    const nextImg = next(currentProductImg);
+
+    imgSet(image, nextImg);
+
+    if(thumbnailsWrapper.style.display !== "none") {
+      thumbnailImgSet(tbn, nextImg);
+    }
+  });
+}
+
+prevImage(productImage, prevBtn);
+nextImage(productImage, nextBtn);
 
 
 
